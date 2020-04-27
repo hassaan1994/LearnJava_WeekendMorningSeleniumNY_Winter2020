101213a9e1cc44e766ab7f696e0061c48cdbd810
Hassaan <hassaannizam1994@gmail.com>
2020-04-27 02:19:46 -0400

    Exception Handling Introduced

--- src/debugging/Calculator.java
+++ src/debugging/Calculator.java
@@ -1,14 +1,27 @@
 package debugging;
-
+import java.util.Scanner;
 public class Calculator {
 
     //Debugging: De+Bug
     public static void main(String[] args) {
-        int number1=55;
-        int number2=65;
+        int number1 = 55;
+        int number2 = 65;
 
-        int summation=number2-number1;
+        int summation = number2 - number1;
         System.out.println("Summation Value Is " + summation);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your Number");
+        int rowNumber=input.nextInt();
+        for (int i=1; i<=rowNumber; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print(j+" ");
+
+            }
+            System.out.println();
+        }
+
+
 
     }
 
--- src/problemsandsolutions/FindLargestNumber.java
+++ src/problemsandsolutions/FindLargestNumber.java
@@ -1,4 +1,18 @@
 package problemsandsolutions;
 
+import javax.swing.*;
+
 public class FindLargestNumber {
+
+    public static void main(String[] args) {
+        int num1=45;
+        int num2=55;
+        int num3=65;
+                if(1>2 && 1>3){
+                    System.out.println("45 is a greater number");
+                }else if (2>1 && 2>3){
+                    System.out.println("55 is a greater number");
+                }else
+            System.out.println("65 is a greater number");
+    }
 }
